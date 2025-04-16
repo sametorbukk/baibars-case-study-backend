d437b3efca6bb3a139a849bc43570617845042f8
Samet Orbuk <orbuk60@gmail.com>
2025-04-16 17:51:21 +0300

    Telemetry Controller created

---
+++ src/main/kotlin/com/example/baibars_case_study_backend/BaibarsCaseStudyBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.baibars_case_study_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BaibarsCaseStudyBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BaibarsCaseStudyBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/baibars_case_study_backend/BaibarsCaseStudyBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.baibars_case_study_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaibarsCaseStudyBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
