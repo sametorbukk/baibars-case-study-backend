8a75dbe3116577089b61af7cf1519aeb1a02e4a3
Samet Orbuk <orbuk60@gmail.com>
2025-04-16 21:27:01 +0300

    gpsCoordinates created as random

--- src/main/kotlin/com/example/baibars_case_study_backend/controller/TelemetryController.kt
+++ src/main/kotlin/com/example/baibars_case_study_backend/controller/TelemetryController.kt
@@ -10,16 +10,18 @@ import kotlin.random.Random
 
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 class TelemetryController {
 
 
     @GetMapping("/telemetry")
     fun getTelemetry(): Telemetry{
+        val latitude = 41.015137 + Random.nextDouble(-0.01, 0.01)
+        val longitude = 28.979530 + Random.nextDouble(-0.01, 0.01)
         return Telemetry(
             Math.round(Random.nextDouble(15.5, 18.9) * 10.0) / 10.0,
             altitude = Random.nextInt(100,200),
-            gpsCoordinates = "41.015137, 28.979530",
+            gpsCoordinates = "%.6f, %.6f".format(latitude, longitude),
             flightTime = LocalTime.of(0, Random.nextInt(0, 59), Random.nextInt(0, 59))
                 .format(DateTimeFormatter.ofPattern("HH:mm:ss"))
         )
