d9c1950007d4c2ffe263ef0028c5b2cdf1c23f7e
Samet Orbuk <orbuk60@gmail.com>
2025-04-16 17:50:25 +0300

    Telemetry Controller created

---
+++ src/main/kotlin/com/example/baibars_case_study_backend/controller/TelemetryController.kt
@@ -0,0 +1,27 @@
+package com.example.baibars_case_study_backend.controller
+
+import com.example.baibars_case_study_backend.entity.Telemetry
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import kotlin.random.Random
+
+
+@RestController
+@RequestMapping("api")
+class TelemetryController {
+
+
+    @GetMapping("/telemetry")
+    fun getTelemetry(): Telemetry{
+        return Telemetry(
+            Math.round(Random.nextDouble(15.5, 17.9) * 10.0) / 10.0,
+            altitude = Random.nextInt(100,200),
+            gpsCoordinates = "41.015137, 28.979530",
+            flightTime = LocalTime.of(0, Random.nextInt(0, 59), Random.nextInt(0, 59))
+                .format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+        )
+    }
+}
---
+++ src/main/kotlin/com/example/baibars_case_study_backend/entity/TelemetryDTO.kt
@@ -0,0 +1,8 @@
+package com.example.baibars_case_study_backend.entity
+
+class Telemetry(
+    val voltage: Double,
+    val altitude: Int,
+    val gpsCoordinates: String,
+    val flightTime: String
+)
